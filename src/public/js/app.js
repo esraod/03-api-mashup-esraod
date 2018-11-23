7db2965417be6793f158bb21e3f0b5a1b99fd461
Esra Oktav Dinler <oktavesra@gmail.com>
2018-11-23 12:42:22 +0100

    :fire: Removed unnecessary console.logs, comments etc. + :zap: Fixed so the clicked suggestions is shown in the input(so the user can see what is searched for) + :sparkles: Fixed the code structure

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -9,20 +9,20 @@ class Mashed {
 
 
     initialize() {
-        // Egenskaper för instanser av den här klassen, används för att referera till samma Node/Element i DOM.
         this.sentinel = document.querySelector('.sentinel');
         this.searchInput = document.querySelector('.search input');
         this.searchBtn = document.querySelector('.search button');
         this.sgstWords = document.querySelectorAll('div#sgst ul');
         this.searchResultsContainer = document.querySelector('.results ul');
 
-        // Frivilligt: för att visa en laddningsindikator!
+        // loading indicator (--not using this--)
         this.loadingIndicator = document.querySelector('.loader');
     }
 
 
-    //Method that sets up our eventlisteners
+    //Method that sets up the eventlisteners
     addEventListeners() {
+
         // Event listener to the search button
         this.searchBtn.addEventListener('click', event =>
         this.search(event, this.searchInput.value)
@@ -36,19 +36,6 @@ class Mashed {
         );
     }
 
-    
-    /*
-        1) Bygg upp en array med anrop (promise) till fetchFlickrPhotos och fetchWordlabWords med searchString
-        Notera: att ordningen du skickar in dessa i spelar roll i steg 3)
-        2) Använd Promise.all för att hantera varje anrop (promise)
-        2 a) then(results) => Om varje anrop lyckas och varje anrop returnerar data
-
-        3) För varje resultat i arryen results, visa bilder från FlickR or ord från WordLab.
-        4 results[0] kommer nu innehålla resultat från FlickR och results[1] resultat från WordLab.
-        5 skapa element och visa dem i DOM:en med metoderna (renderFlickResults och renderWordlabResults)
-
-        2 b) catch() => Om något anrop misslyckas, visa felmeddelande
-    */
 
     /**
     * Method (used as a callback) to handle searches
@@ -56,32 +43,26 @@ class Mashed {
     * @param {*} event Det event som gjorde att denna callback anropades
     * @param {*} [searchString=null] Den söksträng som användaren matat in i fältet, är null by default
     */
+
     search(event, searchString = null) {
         event.preventDefault();
 
-        // Om söksträngen inte är tom och är definierad så ska vi söka
+        // If the search string is not empty and is defined then we are going to search
         if (this.checkSearchInput(searchString)) {
-            // Console loggar det vi sökt på som kommer trigga sökningen etc.
-            console.log(`Trigga sökning med ${searchString}`);
+            // Console logging what has been searched for (what is going to trig the search etc.)
+            console.log(`Trigg the search with ${searchString}`);
+            // Shows what is searched for (Added this for making the user able to see what they recently searched for if the search was from clicking on a suggestion).
+            this.searchInput.value=(`${searchString}`);
 
-
-            // 1) Array med anrop(promise) till de två fetch'n 
+            // Array with promise to the two fetch's
             const resultArray = [this.fetchFlickrPhotos(searchString),this.fetchWordlabWords(searchString)];
 
-
-            // 2) Promise.all för att hantera anrop(promoise)
+            // Promise.all for handle promise
             Promise.all(resultArray)
             
-            //2a) then(resuluts) => om varje anrop lyckas och anropen retunerar data
+            // Then (results) => If each call succeeds and the call returns data
             .then(results => {
-                //this.fetchFlickrPhotos(results[0]);
-                //this.fetchWordlabWords(results[1]);
-                //console.log("working console in my Promise.all"); 
-                //console.log(results);
-                console.log(results[1].status)
-                //console.log(results[1])
-                //console.log(results[1].json())
-                //return Promise.all([results[0].json(), results[1].json()]); 
+
                 if (results[0].status === 200 && results[1].status === 200) {
                     return Promise.all([results[0].json(), results[1].json()]);;
                 } else if (results[0].status === 200) {
@@ -89,56 +70,44 @@ class Mashed {
                 } 
             })
 
-            .then(data =>{ 
+            // Then (data) 
+            .then(data => { 
+
                this.renderFlickrResults(data[0]);
+
+               // Clears the previous suggestions
                document.querySelector("div#sgst ul").innerText="";
-               //this.renderWordlabResults(data[1]);
-                if (data[1]){
-                    this.renderWordlabResults(data[1]);
+            
+               if (data[1]) {
+                   this.renderWordlabResults(data[1]);
                 }
-               console.log(data[0]);
+
             })
 
-            //  3) För varje resultat i arryen results, visa bilder från FlickR or ord från WordLab.
-
-            // 4) results[0] kommer nu innehålla resultat från FlickR och results[1] resultat från WordLab.
-
-            // 5) skapa element och visa dem i DOM:en med metoderna (renderFlickResults och renderWordlabResults)
-
-            // 2b) catch() => Om något anrop misslyckas visa felmedelande
-
-
+            // If any search failes (no related photos is found)
             .catch( err => {
-                //alert("nothing matched");
+                // Clears the suggestions (seeing nothing of the previous suggestions)
                 document.querySelector("div#sgst ul").innerText="";
+                // Sets attribute style to 1 column-count
                 document.querySelector(".results ul").setAttribute('style', "column-count: 1;")
+                // Clears the inside of the search result container (Like previous results.)
                 this.searchResultsContainer.innerHTML ="";
+                // Adds the list item with the Message telling nothing was found (search for something else)
                 this.searchResultsContainer.insertAdjacentHTML('afterbegin','<li class="result"> <p>Noting matched your search,</br> <span id="yellowtag">try search for something else</span></p></li>');
+                // Clears the input 
                 this.searchInput.value="";
-               
-                
             });
-           /* .catch(() => {
-
-                //alert("nothing matched");
-                
-                //this.searchResultsContainer.innerHTML ="";
-                //this.searchResultsContainer.insertAdjacentHTML('afterbegin','<li class="result"> <p>Noting matched your search,</br> <span id="yellowtag">try search something else</span></p></li>');
-                //this.searchInput.value="";
-                
-                
-            
-            });
-            */
         
-
+        // Else
         } else {
-            // Om söksträngen iär tom och inte definierad
+            // If the seach string is empty
+            // -- Not used --
             console.log(
             `Söksträngen är tom, visa ett meddelande eller bara returnera`
             );
             return;
         }
+
     }
 
 
@@ -148,6 +117,8 @@ class Mashed {
    * @param {*} searchString Söksträngen som matats in av användaren
    * @returns Boolean (true/false)
    */
+
+
     checkSearchInput(searchString) {
         return searchString && searchString.trim().length > 0;
     }
@@ -160,11 +131,14 @@ class Mashed {
    * @param {*} searchString Söksträngen som matats in av användaren
    * @returns {Promise} Ett fetch() Promise
    */
-    fetchFlickrPhotos(searchString){
-        let flickrAPIkey = `42e291bad00fd4332215d3336cc52534`; // Din API-nyckel här
-        let flickerAPIRootURL = `https://api.flickr.com/services/rest/?`; // Grundläggande delen av Flickr's API URL
 
-        // Olika sökparametrar som behövs för Flickr's API. För mer info om detta kolla i Flickrs API-dokumentation
+
+    fetchFlickrPhotos(searchString) {
+
+        let flickrAPIkey = `42e291bad00fd4332215d3336cc52534`; // API - Key
+        let flickerAPIRootURL = `https://api.flickr.com/services/rest/?`;
+
+        // Different search parameters needed for Flickr's API. For more information about this check out the Flickr API-documentation
         let flickrQueryParams = `&method=flickr.photos.search&api_key=${flickrAPIkey}&text=searchString&extras=url_q, url_o, url_m&format=json&tags=${searchString}&license=2,3,4,5,6,9&sort=relevance&parse_tags=1&nojsoncallback=1`;
         let flickrURL = `${flickerAPIRootURL}${flickrQueryParams}`;
 
@@ -178,8 +152,9 @@ class Mashed {
    * @param {*} searchString Söksträngen som matats in av användaren
    * @returns {Promise} Ett fetch() Promise
    */
+  
     fetchWordlabWords(searchString) {
-        let wordLabAPIkey = `31da8b2236a3ad990c9707422568cee5`; // Din API-nyckel här
+        let wordLabAPIkey = `31da8b2236a3ad990c9707422568cee5`; // API - Key
         let wordLabURL = `http://words.bighugelabs.com/api/2/${wordLabAPIkey}/${searchString}/json`;
 
         return fetch(wordLabURL);
@@ -190,36 +165,41 @@ class Mashed {
      * Metod som skapar bild-element och relaterade element för varje sökresultat mot Flickr
      *
      * @param {Object} data Sökresultaten från Flickr's API.
-     */
+    */
+
     renderFlickrResults(data) { 
+
+        // Clears the search result container
         this.searchResultsContainer.innerHTML ="";
+
+        // "Removes" the Style attribute for the results ul (used to remove column-count 1, which is used in the start and in the catch).
         document.querySelector(".results ul").setAttribute('style', "");
+    
+        // Loops 15 photos from Flickr
         for (let i = 0; i < 15; i++){
+            // Loops 15 photos wrapped inside of an a-tag(link) which is making the user able to click and open the photo on Flickr 
             this.searchResultsContainer.insertAdjacentHTML('afterbegin', `<a href="https://www.flickr.com/photos/${data.photos.photo[i].owner}/${data.photos.photo[i].id}/" target="_blank"><img src="https://farm${data.photos.photo[i].farm}.staticflickr.com/${data.photos.photo[i].server}/${data.photos.photo[i].id}_${data.photos.photo[i].secret}_c.jpg"></a>`)
         }; 
         
     }
     
 
-
     /**
      * Metod som skapar ord-element för relaterade sökord som kommer från Wordlabs API
      *
      * @param {Object} data Sökresultaten från Flickr's API.
-     */
+    */
+
     renderWordlabResults(data) {
-        //this.sgstWords.innerHTML="";
-        document.querySelector("div#sgst ul").innerText="";
-        console.log(data);
         
+        // Clears the previous suggestions
+        document.querySelector("div#sgst ul").innerText="";
+        
+        // Loops 3 suggestions (synonyms) for the searched string
         for (let i = 0; i < 3; i++){
             if (data.noun.syn[i].length > 0){   
                 document.querySelector("div#sgst ul").insertAdjacentHTML('afterbegin', `<li><a href="#">${data.noun.syn[i]}</a></li>`);
             }
-           
-            
-            //this.sgstWords.insertAdjacentHTML =('afterbegin', `<li><a href="#">${data.noun.syn[i]}</a></li>`)
-            //document.querySelector("div#sgst ul").insertAdjacentHTML('afterbegin', `<li><a href="#">${data.noun.syn[i]}</a></li>`);
         }; 
        
     }
